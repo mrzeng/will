edf9ca843274653361f2e99d5a983052dea482a7
闫帅 <sartisty@gmail.name>
2014-07-06 16:09:43 +0800

    [bug]修复run.bat classpath应以;分割的bug [improvement]设置run.sh开启失败终止功能

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 cmd /c mvn clean package
 cmd /c mvn dependency:copy-dependencies -DoutputDirectory=target/lib
 cd target
-java -cp lib\*:.\* io.github.yanshuai.will.Application
+java -cp lib\*;.\* io.github.yanshuai.will.Application
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -ex
+
 mvn clean package
 mvn dependency:copy-dependencies -DoutputDirectory=target/lib
 cd target
-java -cp lib\*:.\* io.github.yanshuai.will.Application
+java -cp lib/*:./* io.github.yanshuai.will.Application
