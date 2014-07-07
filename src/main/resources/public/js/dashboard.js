842a5dcfda246f37012f643adcf3f2f39f542772
闫帅 <sartisty@gmail.name>
2014-07-07 22:37:20 +0800

    格式化代码

--- src/main/resources/public/js/dashboard.js
+++ src/main/resources/public/js/dashboard.js
@@ -1,26 +1,26 @@
-$(function () {
+$(function() {
 
-	var sparkOptions = {
-		type: 'bar'
-		, height: '35px'
-		, barWidth: '5px'
-		, barSpacing: '2px'
-	};
+  var sparkOptions = {
+    type: 'bar'
+    , height: '35px'
+    , barWidth: '5px'
+    , barSpacing: '2px'
+  };
 
-	$('#total-visits').sparkline(
-		'html', $.extend (sparkOptions, { barColor: '#e5412d' })
-	);
+  $('#total-visits').sparkline(
+      'html', $.extend(sparkOptions, {barColor: '#e5412d'})
+      );
 
-	$('#new-visits').sparkline(
-		'html', $.extend (sparkOptions, { barColor: '#444' })
-	);
+  $('#new-visits').sparkline(
+      'html', $.extend(sparkOptions, {barColor: '#444'})
+      );
 
-	$('#unique-visits').sparkline(
-		'html', $.extend (sparkOptions, { barColor: '#999' })
-	);
+  $('#unique-visits').sparkline(
+      'html', $.extend(sparkOptions, {barColor: '#999'})
+      );
 
-	$('#revenue-visits').sparkline(
-		'html', $.extend (sparkOptions, { barColor: '#5cb85c' })
-	);
+  $('#revenue-visits').sparkline(
+      'html', $.extend(sparkOptions, {barColor: '#5cb85c'})
+      );
 
 });
--- src/main/resources/public/js/donut.js
+++ src/main/resources/public/js/donut.js
@@ -1,26 +1,30 @@
-$(function () {
-	
-    if (!$('#donut-chart').length) { return false; }
+$(function() {
 
-	donut ();
+  if (!$('#donut-chart').length) {
+    return false;
+  }
 
-	$(window).resize (App.debounce (donut, 325));
+  donut();
+
+  $(window).resize(App.debounce(donut, 325));
 
 });
 
-function donut () {
-	$('#donut-chart').empty ();
+function donut() {
+  $('#donut-chart').empty();
 
-	Morris.Donut({
-        element: 'donut-chart',
-        data: [
-            {label: 'Direct', value: 25 },
-            {label: 'Referrals', value: 40 },
-            {label: 'Search engines', value: 25 },
-            {label: 'Unique visitors', value: 10 }
-        ],
-        colors: App.chartColors,
-        hideHover: true,
-        formatter: function (y) { return y + "%" }
-    });
+  Morris.Donut({
+    element: 'donut-chart',
+    data: [
+      {label: 'Direct', value: 25},
+      {label: 'Referrals', value: 40},
+      {label: 'Search engines', value: 25},
+      {label: 'Unique visitors', value: 10}
+    ],
+    colors: App.chartColors,
+    hideHover: true,
+    formatter: function(y) {
+      return y + "%";
+    }
+  });
 }
